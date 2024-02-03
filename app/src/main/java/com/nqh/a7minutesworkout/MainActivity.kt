89d5f19600b7f15b1f6b6da65893a7dc2499729f
HA1211 <quanghiep2408@gmail.com>
2024-02-03 18:35:38 +0700

    Initial commit

---
+++ app/src/main/java/com/nqh/a7minutesworkout/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.nqh.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.nqh.a7minutesworkout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.flStart.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Hi", Toast.LENGTH_LONG).show()
+        }
+    }
+}
