77a7a248d4d3bdd0e990e166403802539742414e
HA1211 <quanghiep2408@gmail.com>
2024-02-06 21:14:08 +0700

    exerciseTimer

--- app/src/main/java/com/nqh/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/nqh/a7minutesworkout/ExerciseActivity.kt
@@ -3,6 +3,7 @@ package com.nqh.a7minutesworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import com.nqh.a7minutesworkout.databinding.ActivityExerciseBinding
 import com.nqh.a7minutesworkout.databinding.ActivityMainBinding
@@ -13,6 +14,8 @@ class ExerciseActivity : AppCompatActivity() {
 
     private lateinit var restTimer: CountDownTimer
     private var restProgress = 0
+    private var restProgressExercise = 0
+    
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,10 +48,30 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                Toast.makeText(this@ExerciseActivity, "Done", Toast.LENGTH_LONG).show()
+                binding.flProgressBar.visibility = View.INVISIBLE
+                binding.tvTitle.setText("Exercise Name")
+                exerciseTimer()
             }
 
         }.start()
     }
+
+    private fun exerciseTimer(){
+        binding.progressExercise.progress = restProgressExercise
+
+        restTimer = object: CountDownTimer(30000, 1000){
+            override fun onTick(p0: Long) {
+                restProgressExercise++
+                binding.progressExercise.progress = 30 - restProgressExercise
+                binding.tvTimerExercise.text = (30 - restProgressExercise).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity,"Done", Toast.LENGTH_LONG).show()
+            }
+        }.start()
+    }
+
+
 
 }
