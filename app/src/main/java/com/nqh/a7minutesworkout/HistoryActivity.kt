9d14ac1851b69df4723ba93e9ba99e015ecdbff9
HA1211 <quanghiep2408@gmail.com>
2024-02-22 02:38:28 +0700

    update

--- app/src/main/java/com/nqh/a7minutesworkout/HistoryActivity.kt
+++ app/src/main/java/com/nqh/a7minutesworkout/HistoryActivity.kt
@@ -3,7 +3,9 @@ package com.nqh.a7minutesworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.nqh.a7minutesworkout.databinding.ActivityHistoryBinding
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -34,8 +36,24 @@ class HistoryActivity : AppCompatActivity() {
     private fun getAllCompletedDates(historyDao: HistoryDao){
         lifecycleScope.launch {
             historyDao.fetchAllDate().collect{allCompletedDatesList ->
-                for (i in allCompletedDatesList){
-                    Log.e("Date: ",""+i)
+                if(allCompletedDatesList.isNotEmpty()){
+                    binding.rvHistory.visibility = View.VISIBLE
+                    binding.tvNoDataAvailable.visibility = View.INVISIBLE
+
+                    binding.rvHistory.layoutManager = LinearLayoutManager(this@HistoryActivity)
+
+                    val dates = ArrayList<String>()
+                    for(date in allCompletedDatesList){
+                        dates.add(date.date)
+                    }
+                    val historyAdapter = HistoryAdapter(dates)
+
+                    binding.rvHistory.adapter = historyAdapter
+
+                }else{
+                    binding.tvHistory.visibility = View.GONE
+                    binding.rvHistory.visibility = View.GONE
+                    binding.tvNoDataAvailable.visibility = View.VISIBLE
                 }
 
             }
